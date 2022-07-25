a9ec2ba565e4ad25d41d64f6c52dbdd2702ea1c7
KristianColville1 <kristiancolville1@gmail.com>
2022-07-25 20:01:28 +0100

    Added basic post model class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+  title = models.TextField(max_length=80)
+  slug = models.SlugField(unique=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+  post = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
+
+  
+  
