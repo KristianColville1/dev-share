babe1dfbe401d96cfba164c2eb3fd107fb55f045
KristianColville1 <kristiancolville1@gmail.com>
2022-07-27 15:45:14 +0100

    Created a post event model to handle post events for feed app

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class PostEvent(models.Model):
+  """
+  Created when a user triggers a post event.
+  """
+  instigator = models.ForeignKey(
+      User, on_delete=models.CASCADE, related_name='instigated_post_event')
+  post = models.ForeignKey(
+      Post, on_delete=models.CASCADE, related_name='post_event')
+  created_on = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+    """
+    Data used to order events.
+    """
+    ordering = ['-created_on']
+  
+  def __str__(self):
+    return f'${self.instigator} created a post on ${self.created_on}'
+  
